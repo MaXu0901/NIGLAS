b3a9afa85569d39f418ba9ab548f6ecb80f5c499
maxu0901 <maxu0901@gmail.com>
2023-09-27 15:01:53 +0800

    Add a dataset

---
+++ R/data.R
@@ -0,0 +1,11 @@
+#' DOM molecules data
+#'
+#' Example data of a DOM compositional table (100 samples by 100 DOM molecules)
+#'
+#' @format The format for DOM data is a data.frame with samples in the rows and DOM molecules in the columns.
+#' @references Hu A, Choi M, Tanentzap AJ, Liu J, Jang K-S, Lennon JT, Liu Y, Soininen J, Lu X, Zhang Y, Shen J, Wang J. 2021.
+#' Quantifying microbial associations of dissolved organic matter under global change. \emph{bioRxiv}.
+#' @references Hu A, Choi M, Tanentzap AJ, Liu J, Jang K-S, Lennon JT, Liu Y, Soininen J, Lu X, Zhang Y, Shen J, Wang J. 2022.
+#' Quantifying the associations between dissolved organic matter and microbes under global change. \emph{Nature communications}.
+#' @examples data(DOM.data)
+"DOM.data"
