43ca60ee1a6f515affc71e7a7cb87a0c4fb4d7cf
maxu0901 <maxu0901@gmail.com>
2023-09-28 14:23:15 +0800

    Add functions

---
+++ R/chemoDiv.R
@@ -0,0 +1,19 @@
+#' @title FUNCTION_TITLE
+#' @description FUNCTION_DESCRIPTION
+#' @param chem_dataset PARAM_DESCRIPTION
+#' @return OUTPUT_DESCRIPTION
+#' @details DETAILS
+#' @examples 
+#' \dontrun{
+#' if(interactive()){
+#'  #EXAMPLE1
+#'  }
+#' }
+#' @rdname chemoDiv
+#' @export 
+chemoDiv <- function(chem_dataset) {
+  data.frame(Sample = rownames(chem_dataset),
+             shannon = diversity(chem_dataset,index = "shannon"),
+             simpson = diversity(chem_dataset,index = "simpson"),
+             invsimpson = diversity(chem_dataset,index = "invsimpson"))
+}
